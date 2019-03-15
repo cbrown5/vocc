fb004d82cc7a6052a5479423520d966b29161efa
Sean Anderson <sean@seananderson.ca>
2019-03-15 16:44:45 -0700

    Fix all issues to pass R CMD check

--- R/calcvelocity.R
+++ R/calcvelocity.R
@@ -1,26 +1,22 @@
 #' Calculate velocity from the spatial gradient and temporal trends
 #'
-#' @usage calcvelociy(grad, slope, y_dist = 111.325)
-#'
 #' @param grad \code{data.frame} returned from \code{spatialgrad}
 #' @param slope \code{data.frame} returned from \code{calcslope}
-#' @param y_diff \code{numeric} giving units (degrees latitude) for the coordinate
+#' @param y_dist \code{numeric} giving units (degrees latitude) for the coordinate
 #'
 #' @return \code{data.frame} giving velocity.
-#'
-#' @details
 #'
 #' @author Christopher J. Brown
 #' @examples
 #' data(sst)
 #' slopedat <- calcslope(sst)
-#' allyears <- rep(1, nlayers(sst))
-#' mnsst <- stackApply(sst, indices = allyears, fun = mean)
+#' allyears <- rep(1, raster::nlayers(sst))
+#' mnsst <- raster::stackApply(sst, indices = allyears, fun = mean)
 #' spatx <- spatialgrad(mnsst)
 #' velodf <- calcvelocity(spatx, slopedat)
-#' r <- raster(sst)
+#' r <- raster::raster(sst)
 #' r[velodf$icell] <- velodf$velocity
-#' plot(r)
+#' raster::plot(r)
 #' @rdname calcvelociy
 #' @export
 
@@ -31,7 +27,9 @@ calcvelocity <- function(grad, slope, y_dist = 111.325){
     grd$WE <- ifelse(is.na(grd$WEold) == TRUE, 0, grd$WEold)
     grd$NAsort <- ifelse(abs(grd$NS)+abs(grd$WE) == 0, NA, 1)
     grd$Grad <- grd$NAsort * sqrt((grd$WE^2) + (grd$NS^2))
-    velocity <- data.frame(x = slope$x, y = slope$y, temporal_trend = slope$slope, spatial_gradient = grd$Grad, NSgrad = grad$NS, WEgrad = grad$WE, angle = grad$angle, w = slope$w, icell = grad$icell)
-    velocity$velocity <- with(velocity, temporal_trend/spatial_gradient)
-return(velocity)
+    velocity <- data.frame(x = slope$x, y = slope$y, temporal_trend = slope$slope,
+        spatial_gradient = grd$Grad, NSgrad = grad$NS, WEgrad = grad$WE,
+        angle = grad$angle, w = slope$w, icell = grad$icell)
+    velocity$velocity <- velocity$temporal_trend/velocity$spatial_gradient
+    velocity
 }
--- R/internals.R
+++ R/internals.R
@@ -1,20 +1,17 @@
-#' @rdname .mnwm
 .mnwm <- function(d1, d2, d3, d4, d5, d6){
-    X <- sum(c(d1, d2*2, d3, d4, d5*2, d6), na.rm = T)
+    X <- sum(c(d1, d2*2, d3, d4, d5*2, d6), na.rm = TRUE)
     w <- sum(c(1,2,1,1,2,1) * is.finite(c(d1, d2, d3, d4, d5, d6)))
-   return(X/w)
+    X/w
 }
 
-#' @rdname .ang
 .ang <- function(dx, dy){
 	ifelse(dy < 0, 180 + CircStats::deg(atan(dx/dy)),
         ifelse(dx < 0, 360 + CircStats::deg(atan(dx /dy )), CircStats::deg(atan(dx/dy))))
 	}
 
-#' @rdname .getmindist
-.getmindist <- function(inum, dat, distfun = st_distance, ...){
-	dtemppre <- dat %>% dplyr::filter(prefact == inum)
-	dtemppost <- dat %>% dplyr::filter(postfact == inum)
+.getmindist <- function(inum, dat, distfun = sf::st_distance, ...){
+	dtemppre <-  dplyr::filter(dat, .data$prefact == inum)
+	dtemppost <-  dplyr::filter(dat, .data$postfact == inum)
 
 	if (nrow(dtemppost) == 0){
 		dtemppre$dist <- Inf
